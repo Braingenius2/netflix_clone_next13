184f514ef74ae24a49d9f5f28984776e7b2269d9
Braingenius2 <braingenius2@gmail.com>
2024-02-06 01:27:23 +0300

    Configure TailwindCSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
-        <h1>My Netflix Clone Project</h1>
+        <h1 className='text-3xl text-red-600'>Netflix Clone</h1>
       </main>
     </>
   )
