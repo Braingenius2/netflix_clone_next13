3b8e8f899e88478aa0031487bab950fe52efc60e
Braingenius2 <braingenius2@gmail.com>
2024-02-18 02:46:57 +0300

    Add Auth page

---
+++ pages/auth.tsx
@@ -0,0 +1,20 @@
+const Auth = () => {
+  return (
+    <div className="relative flex h-full w-full bg-[url('/images/hero1.jpg')] bg-cover bg-no-repeat bg-center">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="Netflix Logo" className="h-12"/>
+        </nav>
+        <main className="flex justify-center">
+          <div className="px-16 py-16 bg-black bg-opacity-70 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl">
+              Sign in
+            </h2>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
