6793372d8ee3d82497989815d55234690d2e6bb1
Braingenius2 <braingenius2@gmail.com>
2024-03-21 15:13:46 +0300

    Add access funtionality to homepage. If no user is logged in, it redirects to auth page. Only logged in users can access homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,35 @@
+import { NextPageContext } from 'next'
+import { getSession, signOut } from 'next-auth/react'
 import Head from 'next/head'
+
+export async function getServerSideProps (context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: { 
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Netflix-clone</title>
         <meta name="description" content="A clone for learning purposes" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main>
         <h1 className='text-3xl text-red-600'>Netflix Clone</h1>
+        <button onClick={ () => signOut()} className='h-10 w-full bg-white'>Logout!</button>
       </main>
     </>
   )
